e4e9f70cd4765dc45571ce32cc3ce8cb741bd2a2
marsha <marshaannon@gmail.com>
2020-06-21 16:00:21 -0400

    created class for post with constructor attr.

---
+++ js/models/post.js
@@ -0,0 +1,10 @@
+class Post {
+    constructor(){
+        this.title = title
+        this.content = content
+        this.author_name = author_name
+        this.likes = likes
+        this.created_at = created_at
+    }
+
+}
