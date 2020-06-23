1059c87e607adad01db4c41fefa486f9cb42d141
marsha <marshaannon@gmail.com>
2020-06-23 15:40:22 -0400

    Added Post.all.push(this) and this.renderPost() to the constructor

--- js/models/post.js
+++ js/models/post.js
@@ -1,10 +1,28 @@
 class Post {
-    constructor(){
+    
+    static all = []
+    
+    constructor(id, title, content, author_name, likes, created_at){
+        this.id = id
         this.title = title
         this.content = content
         this.author_name = author_name
         this.likes = likes
         this.created_at = created_at
+        this.renderPost()
+        Post.all.push(this)    
+    }
+
+    postHTML(){
+
+    }
+
+    renderPost(){
+        console.log("this works??")
     }
 
 }
+// render all posts to the page
+// update post with likes
+// access to show post page
+// likes button on each post
