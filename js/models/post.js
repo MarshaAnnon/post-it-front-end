e51254c33bf92133e08e244b2cea1a0f0a9ce13e
marsha <marshaannon@gmail.com>
2020-06-23 21:16:48 -0400

    Added postHTML and renderPost

--- js/models/post.js
+++ js/models/post.js
@@ -1,5 +1,4 @@
 class Post {
-    
     static all = []
     
     constructor(id, title, content, author_name, likes, created_at){
@@ -9,18 +8,37 @@ class Post {
         this.author_name = author_name
         this.likes = likes
         this.created_at = created_at
+        Post.all.push(this)//collecting all of the objects removes the need to call to call the fetch again, by storing the objects in the frontend. Depends on how the data is being loaded. Make sure the created data is being added to the all array. Make sure the static = [] keeps up with the creation.
         this.renderPost()
-        Post.all.push(this)    
+            
     }
 
+//need a helper method for this.created_at
     postHTML(){
-
+        return (`
+        <H3>${this.title}</H3>
+        <h5>${this.created_at}</h5> 
+        <p>${this.content}</p>
+        <h5>${this.author_name}</h5>
+        <button>${this.likes}</button>
+        `)
     }
 
     renderPost(){
-        console.log("this works??")
+        
+    
+        const postContainer = document.getElementById("post-container")
+        const postCard = document.createElement("div")
+        
+        postCard.classList.add("post-card")
+        
+        postCard.innerHTML += this.postHTML()
+        postContainer.appendChild(postCard)
+        /*postCard.addEventListener("click", e => {
+            if (e.target.className === "like") this.likeBtn(e)}*/
+        
     }
-
+        
 }
 // render all posts to the page
 // update post with likes
