a1c199185212f9f4fb0eb0b5ee0375f10d95b27d
marsha <marshaannon@gmail.com>
2020-06-23 21:15:25 -0400

    Fixed DOMContentLoaded

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-document.addEventListener("domContentLoaded", (e) =>{
-    
+document.addEventListener("DOMContentLoaded", (e) =>{
+    console.log("dom loaded")
+
     fetch("http://localhost:3000/posts")
     .then(resp => resp.json())
     .then(posts => {
@@ -9,14 +10,5 @@ document.addEventListener("domContentLoaded", (e) =>{
         })
     })
 
-    //fetch(`http://localhost:3000/posts/${"id"}`)
-    //.then(resp => resp.json())
-    //.then(post => {
-    //    postMessage.forEach(post => {
-    //        const { id, name, title, content, author_name, likes } = post
-    //        new Post = (id, name, title, content, author_name, likes)
-    //    })
-    //})
-    
 })
 
