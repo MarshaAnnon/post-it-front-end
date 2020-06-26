49d46e3a759f5a9f7c53a919720b8e8236edefb5
marsha <marshaannon@gmail.com>
2020-06-26 09:21:12 -0400

    file reorganization

---
+++ js/src/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", (e) =>{
+
+    document.getElementById('form').addEventListener("submit", API.addPosts)
+    document.querySelectorAll("h3")
+
+    API.addPosts()
+})
+    
+
+
+
+    
+
+    
+
+
+
+
+
+
+
