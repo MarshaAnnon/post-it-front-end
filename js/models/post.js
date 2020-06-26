35058801def87e27abbe38e114dc3c54a9ef8750
marsha <marshaannon@gmail.com>
2020-06-26 09:14:28 -0400

    Added showPost(), changed eventListener in renderPost

--- js/models/post.js
+++ js/models/post.js
@@ -9,19 +9,21 @@ class Post {
         this.likes = likes
         this.created_at = created_at
         Post.all.push(this)//collecting all of the objects removes the need to call to call the fetch again, by storing the objects in the frontend. Depends on how the data is being loaded. Make sure the created data is being added to the all array. Make sure the static = [] keeps up with the creation.
-        this.renderPost()
-            
+        this.renderPost()        
     }
 
 //need a helper method for this.created_at
 //need to add a conditional to the like button so it only shows on the post show page and not with every post on the index page
     postHTML(){
         return (`
-        <H3>${this.title}</H3>
+        <div id="${this.id}">
+        <h3 class="title-link"><span>${this.title}</span></h3>
         <h5>${this.created_at}</h5> 
         <p>${this.content}</p>
         <h5>${this.author_name}</h5>
-        <button>${this.likes}</button>
+        <h5>${this.likes}</h5>
+        <button onclick=API.js.likePost() type="button" id="like-button">Like</button>
+        </div>
         `)
     }
 
@@ -31,13 +33,24 @@ class Post {
         postCard.classList.add("post-card")
         postCard.innerHTML += this.postHTML()
         postContainer.appendChild(postCard)
+        postCard.addEventListener("click", (e) => {
+            if (e.target.parentElement.classList.contains("title-link")) this.showPost(e)
+        })
         /*postCard.addEventListener("click", e => {
             if (e.target.className === "like") this.likeBtn(e)}*/ 
     }
-        
-}
-// render all posts to the page
-// update post with likes
-// access to show post page
-// likes button on each post
 
+    showPost(e) {
+        const postContainer = document.getElementById("post-container")
+        const postID = parseInt(e.target.parentElement.parentElement.id)
+        const postForm = document.getElementById("post-form")
+        postContainer.innerHTML = ""
+        postForm.innerHTML = ""
+        
+        API.addPost(postID)
+    }
+
+}
+
+
+    
