e5eeb9cd94ea7f4cccd2010dc2d6af9e42bc8ba5
marsha <marshaannon@gmail.com>
2020-06-26 09:16:56 -0400

    Added static likePost() method and mad a start on the comment fetch

--- js/services/API.js
+++ js/services/API.js
@@ -12,14 +12,48 @@ class API {
         })
     }
 
-    static addPost(e){
+    static addPost(id){
+        // write our fetch and send it to our back end
+  
+        fetch(`http://localhost:3000/posts/${id}`)
+        // grab our fetch response
+        .then(resp => resp.json())
+        .then(post => {
+            const {title, content, author_name, likes, created_at} = post
+            new Post(title, content, author_name, likes, created_at)
+           
+        })
+    }
+
+    static likePost(){
+        const postID = parseInt(event.currentTarget.parentElement.id)
+        let likes = parseInt(event.currentTarget.parentElement.querySelector(".likes").innerText)
+        likes ++
+        event.currentTarget.parentElement.querySelector(".likes").innerText = likes //this could go on 47
+        let likeData = {
+            'likes': likes   
+        }
+   
+    fetch(`http://localhost:3000/posts/${postID}`, {
+            method: 'PATCH',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(likeData)
+        })
+        // grab our fetch response
+        .then(resp => resp.json())
+        
+    }
+
+
+    static addComment(e){
         e.preventDefault()
         // capture our form data
         let data = {
         
-            'title': e.target.title.value,
             'content': e.target.content.value,
-            'author_name': e.target.author_name.value,
+            'name': e.target.name.value,
         }
         // write our fetch and send it to our back end
         
@@ -39,3 +73,6 @@ class API {
         })
     }
 }
+
+    
+
