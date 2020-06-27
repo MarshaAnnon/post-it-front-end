e731ca35bcdd21c0e81b28c126e61b515a3127e4
marsha <marshaannon@gmail.com>
2020-06-27 18:47:55 -0400

    Added event listener for comment-form submit

--- js/src/index.js
+++ js/src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", (e) =>{
-
-    document.getElementById('form').addEventListener("submit", API.addPosts)
-    document.querySelectorAll("h3")
-
     API.addPosts()
+    document.getElementById('form').addEventListener("submit", API.addNewPost)
+    document.querySelectorAll("h3")
+    document.getElementById('comment-container').addEventListener("submit", API.addComment)
+
+    
 })
     
 
