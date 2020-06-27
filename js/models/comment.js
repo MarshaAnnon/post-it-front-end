6b0bb1db3fbbfbd8daa76288035df6f158409ef7
marsha <marshaannon@gmail.com>
2020-06-27 18:55:22 -0400

    Added renderComment() method

--- js/models/comment.js
+++ js/models/comment.js
@@ -1,33 +1,36 @@
 class Comment {
+
+    static all = []
     
-    constructor(id, title, content, author_name, likes, created_at){
+    constructor(id, content, name, created_at){
         this.id = id
         this.content = content
         this.name = name
-        this.post_id = post_id
         this.created_at = created_at
-        this.renderComment()       
+        Comment.all.push(this)
     }
     
     static commentHTML(){
-        return (
-            `
-            <div id="comment-form">
-                <form>
-                    <h4>Would you like to add a comment?</h4>
-                    <label for="content"></label><br>
-                    <textarea type="text" name="comment-content" rows="10" cols="80"        placeholder="Spread the love..."> </textarea>
-                    <br>
-                    <label for="name"><h4>Name</h4></label>
-                    <input type="text" name="name" placeholder="your name">
-                    <br>
-                    <br>
-                    <input type="submit" value="Submit">
-                <form>
-            </div>
-
-        `
-        )
+        return (`
+            <h4>Would you like to add a comment?</h4>
+            <form id="comment-form">
+                <label for="content"></label><br>
+                <textarea type="text" name="commentContent" rows="10" cols="60"        placeholder="Spread the love..."> </textarea>
+                <br>
+                <label for="name"><h4>Name</h4></label>
+                <input type="text" name="name" placeholder="your name">
+                <br>
+                <br>
+                <input type="submit" value="Submit">
+            <form>
+        `)
     }
 
+    renderComment(){
+        const postContainer = document.getElementById("post-container")
+        postContainer.innerHTML +=  `<div id="${this.id}">
+                                        <p>${this.content}</p>
+                                        <h5>${this.name}</h5>
+                                    </div>`
+    }
 }
